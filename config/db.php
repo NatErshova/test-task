ec977c05f8bd4b418931786e39a53deb406af9cf
Nataliya Ershova <n.ershova@smileexpo.ru>
2019-06-05 14:18:50 +0300

    подключена бд

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=test',
     'username' => 'root',
-    'password' => '',
+    'password' => 'us05',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
