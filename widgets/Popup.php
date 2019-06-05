bd1a375fa3ce5f1aecd20205b7004cc796893893
Nataliya Ershova <n.ershova@smileexpo.ru>
2019-06-05 16:55:18 +0300

    рандомный выбор приза

--- widgets/Popup.php
+++ widgets/Popup.php
@@ -2,6 +2,9 @@
 namespace app\widgets;
 
 use yii\base\Widget;
+use app\models\Money;
+use app\models\Points;
+use app\models\Stuff;
 
 class Popup extends Widget
 {
@@ -10,6 +13,43 @@ class Popup extends Widget
     }
 
     public function run() {
-        return $this->render('popup');
+        $category = random_int(1, 3);
+        switch ($category) {
+            case 1:
+                $count = Money::find()->count();
+                $flag=true;
+                while($flag) {
+                    $id = random_int(1, $count);
+                    $prize = Money::findById($id);
+                    if($prize->amt > 0) {
+                        $prize->amt = $prize->amt - 1;
+                        $prize->save();
+                        $flag=false;
+                    }
+                }
+                $text = $prize->val . ' rub';
+                break;
+            case 2:
+                $count = Points::find()->count();
+                $id = random_int(1, $count);
+                $prize = Points::findById($id);
+                $text = $prize->val  . ' points';
+                break;
+            case 3:
+                $count = Stuff::find()->count();
+                $flag=true;
+                while($flag) {
+                    $id = random_int(1, $count);
+                    $prize = Stuff::findById($id);
+                    if($prize->amt > 0) {
+                        $prize->amt = $prize->amt - 1;
+                        $prize->save();
+                        $flag=false;
+                    }
+                }
+                $text = $prize->val;
+                break;
+        }
+        return $this->render('popup', ['text' => $text]);
     }
 }
--- widgets/views/popup.php
+++ widgets/views/popup.php
@@ -1,7 +1,7 @@
 <div id="popup" class="modal modal-popup" role="dialog" tabindex="-1">
     <div class="modal-dialog" role="document">
         <div class="modal-body">
-            <p class="lead">Congratulations, you won </p>
+            <p class="lead">Congratulations, you won <?= $text ?></p>
             <button>Get my gift</button>
             <button type="button" data-dismiss="modal" aria-label="Close">I don't need a gift</button>
         </div>
